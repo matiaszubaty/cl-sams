ad1537f280380cf23d90c7184347f86bec493c08
matiaszubaty <194117738+matiaszubaty@users.noreply.github.com>
2025-07-10 21:07:36 -0300

    se modifica manifiesto

--- sams_default/__manifest__.py
+++ sams_default/__manifest__.py
@@ -41,7 +41,7 @@
     "port": "8069",
     # list of url repos to install in the form 'repo-url directory'
     "git-repos": [
-        "https://github.com/jobiols/cl-sams.git -b 17.0e",
+        "https://github.com/matiaszubaty/cl-sams.git -b 17.0e",
 #        "git@github.com:jobiols/jeo-enterprise.git",
         # OCA
         # "https://github.com/OCA/server-tools oca-server-tools",
